64451b2ba6fc626c5489453563d16749e5d6f1d8
Jstein77 <jordan.stein77@gmail.com>
2022-11-21 20:42:50 -0800

    update run materialization

--- dags/materialization_tasks.py
+++ dags/materialization_tasks.py
@@ -21,8 +21,20 @@ def RunMaterialization():
     def run_materialization():
         mql = MQLClient(TRANSFORM_API_KEY)
         mats = mql.list_materializations()
+        success =0
+        fail = 0
         for mat in mats:
-            mql.materialize(mat.name, timeout=3600)
+            try:
+                materialize_response= mql.materialize(mat.name, timeout=3600, start_time='2018-01-01')
+                print(f"Successfully created table: {materialize_response.schema}.{materialize_response.table} ")
+                success +=1
+            except RuntimeError as err:
+                print("Runtime error",err)
+                fail +=1
+            except Exception as err:
+                print(f"Unexpected {err=}, {type(err)=}")
+                fail += 1
+        print(f"Successful materialization: {success}. Failed materialization: {fail} ")
     run_materialization()
 dag = RunMaterialization()
 
