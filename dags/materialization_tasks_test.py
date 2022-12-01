975021844449d43d1e598334bb153b807d9693c2
Jstein77 <jordan.stein77@gmail.com>
2022-12-01 15:59:42 -0800

    test dag

---
+++ dags/materialization_tasks_test.py
@@ -0,0 +1,39 @@
+import os
+import requests
+import datetime
+import pendulum
+os.environ["GIT_PYTHON_REFRESH"] = "quiet"
+from transform import MQLClient
+from airflow.decorators import dag, task
+from airflow import DAG
+from airflow.models import Variable
+from transform_airflow.operators import MaterializeOperator
+#TRANSFORM_API_KEY = os.environ["TRANSFORM_API_KEY"]
+
+TRANSFORM_API_KEY = Variable.get('TRANSFORM_API_KEY')
+mql = MQLClient(TRANSFORM_API_KEY)
+materialization_list = mql.list_materializations()
+with DAG(
+    dag_id="materialize_sales_metrics",
+    schedule_interval="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    ) as dag:
+        for materialization in materialization_list:
+            mat_operator = MaterializeOperator(
+                task_id= f"{materialization.name}",
+                dag=dag,
+                materialization_name=materialization.name,
+                start_time="2018-01-01",
+                creds={
+                "TRANSFORM_API_KEY": "tfdk-BVtLxr-nfRyPVMfycMJIVmRHoAbVbCJ",
+                "MQL_QUERY_URL": "https://mql-transform-staging.transform.sh/"
+                }
+            )
+
+
+
+
+
+
